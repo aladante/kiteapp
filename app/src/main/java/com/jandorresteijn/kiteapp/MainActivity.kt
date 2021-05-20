06842255268c9e0e7c68866b6c611bc61299bd4f
yung whatever <janjr@base.nl>
2021-05-20 20:49:00 +0200

    add more default files needed in future development

--- app/src/main/java/com/jandorresteijn/kiteapp/MainActivity.kt
+++ app/src/main/java/com/jandorresteijn/kiteapp/MainActivity.kt
@@ -2,17 +2,32 @@ package com.jandorresteijn.kiteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.jandorresteijn.kiteapp.ui.main.LocationViewModel
 import com.jandorresteijn.kiteapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: LocationViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        viewModel = ViewModelProviders.of(this).get(LocationViewModel::class.java)
+        initNavigation()
     }
+
+    private fun initNavigation() {
+        // The NavController
+        val navController = findNavController(R.id.navHostFragment)
+
+        // Connect the navHostFragment with the ActionBar.
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
 }
---
+++ app/src/main/java/com/jandorresteijn/kiteapp/ui/main/LocationViewModel.kt
@@ -0,0 +1,14 @@
+package com.jandorresteijn.kiteapp.ui.main
+
+import androidx.lifecycle.ViewModel
+import java.util.function.DoubleBinaryOperator
+
+class LocationViewModel : ViewModel() {
+    var lat : Double = 0.0
+    var long: Double = 0.0
+
+    fun createLocation(lat : Double, long: Double){
+        this.lat = lat
+        this.long = long
+    }
+}
