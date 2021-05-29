744bbb8950a9c3e16fa4bc6de106b4ed5ac0397f
yung whatever <janjr@base.nl>
2021-05-29 11:57:36 +0200

    working drawer menu

--- app/src/main/java/com/jandorresteijn/kiteapp/MainActivity.kt
+++ app/src/main/java/com/jandorresteijn/kiteapp/MainActivity.kt
@@ -5,15 +5,18 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import com.jandorresteijn.kiteapp.databinding.MainActivityBinding
 import com.jandorresteijn.kiteapp.ui.main.LocationViewModel
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -25,12 +28,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: LocationViewModel
     private  val MULTIPLE_PERMISSION_REQUEST_CODE = 4
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity);
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
 
         viewModel = ViewModelProviders.of(this).get(LocationViewModel::class.java)
+
         initNavigation()
         //load/initialize the osmdroid configuration, this can be done
         val ctx: Context = getApplicationContext();
@@ -40,11 +50,33 @@ class MainActivity : AppCompatActivity() {
 
     private fun initNavigation() {
         val navController = findNavController(R.id.navHostFragment)
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        findViewById<Toolbar>(R.id.toolbar)
-            .setupWithNavController(navController, appBarConfiguration)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.home_fragment,
+                R.id.notification_fragment,
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navcontroller = findNavController(R.id.navHostFragment)
+        return navcontroller.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 
     // check premissions needed to run the application
     private fun checkPermissionsState() {
