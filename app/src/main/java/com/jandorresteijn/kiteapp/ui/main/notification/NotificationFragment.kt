3b6a0ed92a80aa1f9a47301b27da212175b3b709
yung whatever <janjr@base.nl>
2021-05-29 13:02:04 +0200

    add time picker

--- app/src/main/java/com/jandorresteijn/kiteapp/ui/main/notification/NotificationFragment.kt
+++ app/src/main/java/com/jandorresteijn/kiteapp/ui/main/notification/NotificationFragment.kt
@@ -1,5 +1,6 @@
 package com.jandorresteijn.kiteapp.ui.main.notification
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,8 +12,10 @@ import androidx.lifecycle.ViewModelProvider
 import com.jandorresteijn.kiteapp.R
 import com.jandorresteijn.kiteapp.databinding.NotificationFragmentBinding
 import org.osmdroid.views.MapView
+import java.text.SimpleDateFormat
+import java.util.*
 
-class NotificationFragment : Fragment(){
+class NotificationFragment : Fragment() {
 
     private lateinit var notificationViewModel: NotificationViewModel
     private var _binding: NotificationFragmentBinding? = null
@@ -25,7 +28,7 @@ class NotificationFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationViewModel=
+        notificationViewModel =
             ViewModelProvider(this).get(NotificationViewModel::class.java)
 
         _binding = NotificationFragmentBinding.inflate(inflater, container, false)
@@ -35,8 +38,22 @@ class NotificationFragment : Fragment(){
         notificationViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        binding.timePickerbtn.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                textView.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(activity, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        }
+
         return root
     }
+
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
